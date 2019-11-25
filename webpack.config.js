40ff1f8a2da948d409e7a7c38edf3c814c1a84d3
Kevin van Kessel <vankessel@users.noreply.github.com>
2019-11-25 00:01:49 -0800

    Update configuration files and linting rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,44 @@
 const path = require('path');
+const entry_path = path.resolve(__dirname, 'src', 'index.ts');
+const output_path = path.resolve(__dirname, 'dist');
+const node_modules_path = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: __dirname,
-        publicPath: '/dist/',
+    mode: 'production',
+    entry: entry_path,
+    resolve: {
+        extensions: ['.ts', '.js'],
     },
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 loader: 'ts-loader',
-                exclude: /node_modules/,
+                exclude: node_modules_path,
             },
             {
                 test: /\.glsl$/,
                 loader: 'raw-loader',
-                exclude: /node_modules/,
+                exclude: node_modules_path,
             },
             {
                 test: /\.(jpg|png)$/,
                 loader: 'file-loader',
-                exclude: /node_modules/,
+                exclude: node_modules_path,
                 options: {
                     name: '[path][name].[ext]',
-                    publicPath: 'dist/',
+                    publicPath: output_path,
                 },
             },
         ],
     },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: output_path,
         filename: 'bundle.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: __dirname,
+        publicPath: output_path,
     },
 };
